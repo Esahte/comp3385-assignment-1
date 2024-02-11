04d1322db3beeb9cd4328000adf16a36785ae32f
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-02-11 16:28:42 -0400

    Added the about page and created rout for and to it

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Esahtengang Asonganyi</h2>
+    <p>My name is Esahtengang Asonganyi. I am a student at the University of the West Indies Five Island Campus. I am
+        currently enrolled in the Computer Science program. I am taking COMP3385 - Web Development. I am excited to
+        learn about web development and how to create web applications. I am looking forward to learning about HTML,
+        CSS, JavaScript, and PHP. I am also looking forward to learning about Laravel and how to create web applications
+        using the Laravel framework.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', fn() => "My About Page");
+Route::get('/about', [AboutController::class, 'show']);
