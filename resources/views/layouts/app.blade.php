e70075ff2096f43b65b0699c9c709ec01828879a
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-02-12 20:23:52 -0400

    Added Picture and style sheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url('/css/app.css') }}">
     <title>COMP3385 - Lab 1</title>
 </head>
 <body>
@@ -12,6 +13,7 @@
     <h1>COMP3385 - Lab 1</h1>
 </header>
 <main>
+    <img src="{{ url('/images/cool-cover-pictures-timeline (11).jpg') }}" alt="Cartoon Esah">
     @yield('content')
 </main>
 <footer>
